e76961b04e8ad0cc134b6223f7e61b9b7f4593e6
aseoanef <145339971+aseoanef@users.noreply.github.com>
2024-11-14 16:42:52 +0100

    archivos iniciales y primera versión de los models

---
+++ warhammer/models.py
@@ -0,0 +1,56 @@
+from tkinter.constants import CASCADE
+
+from django.db import models
+from django.db.models import DO_NOTHING
+
+
+# Create your models here.
+class Operative(models.Model):
+    name = models.CharField(max_length=240, )
+    movement = models.IntegerField(default=3,)#movement
+    dash = models.IntegerField() #dash
+    apl = models.IntegerField() #action points limit
+    ga = models.IntegerField() #group actions
+    df = models.IntegerField() #Defence
+    sv = models.IntegerField() #save throw
+    w = models.IntegerField() #wounds
+    base= models.IntegerField() #base diametro
+    unique_action = models.ForeignKey('UniqueAction',blank=True,null=True,on_delete=models.DO_NOTHING)
+    gun = models.ForeignKey('Gun',blank=True,on_delete=models.DO_NOTHING)
+
+class UniqueAction(models.Model):
+    name = models.CharField(max_length=240,)
+    cost = models.IntegerField(default=1,)
+    description = models.CharField(max_length=1000,)
+
+class Gun(models.Model):
+    name = models.CharField(max_length=240,)
+    attacks = models.IntegerField()
+    ws = models.IntegerField() #wounds save
+    dmg = models.IntegerField() #damage standard
+    critical_dmg = models.IntegerField() #critical damage
+    special_rules = models.ForeignKey('SpecialRules',blank=True,null=True,on_delete=DO_NOTHING)
+
+class SpecialRules(models.Model):
+    name = models.CharField(max_length=240)
+    description = models.CharField(max_length=1000)
+    modifier = models.IntegerField(blank=True,null=True)
+
+class Army(models.Model):
+    name = models.CharField(max_length=240,null=False,)
+    operatives = models.ForeignKey('Operative',blank=True,null=True,on_delete=DO_NOTHING)
+    ability = models.ForeignKey('Ability', blank=True, on_delete=models.DO_NOTHING)
+    faction = models.CharField(max_length=240,)
+
+class CustomArmy(models.Model):
+    army = models.ForeignKey('Army',on_delete=CASCADE)
+    operative = models.ForeignKey('Operative',on_delete=CASCADE)
+
+class OperativeGuns(models.Model):
+    operative = models.ForeignKey('Operative',on_delete=CASCADE)
+    gun = models.ForeignKey('Gun',on_delete=CASCADE)
+
+class Ability(models.Model):
+    name = models.CharField(max_length=240,)
+    description = models.CharField(max_length=1000)
+    cost = models.IntegerField()
