644bbac34f7cc4b49722f0ab9fe63c12c0566f98
anxo seoane fernandez <aseoanef23@fpcoruna.afundacion.org>
2024-11-22 18:49:04 +0100

    añadidos get para guns

--- warhammer/endpoints.py
+++ warhammer/endpoints.py
@@ -1,8 +1,7 @@
 import json
 
 from django.http import JsonResponse
-
-from .models import Operative
+from .models import Operative, Gun, SpecialRule
 from django.views.decorators.csrf import csrf_exempt
 
 
@@ -39,3 +38,71 @@ def operative(request):
         return JsonResponse(json_response, safe=False)
     else:
         return JsonResponse({'error': 'Unsupported HTTP method'}, status=405)
+
+
+def gun(request):
+    # Manejar la solicitud GET
+    if request.method == 'GET':
+        # checkea si tiene nombre en el header y filtrar en caso positivo
+        if request.headers.get("gunName") != None:
+            gunName = request.headers.get("gunName")
+            try:
+                all_rows = Gun.objects.filter(name=gunName)
+            except all_rows.DoesNotExist:
+                return JsonResponse({"error": "Gun was not found"}, status=404)
+            json_response = []
+            for row in all_rows:
+                rules = row.special_rule.all()
+                ruleses = []
+                for rule in rules:
+                    ruleses.append(rule.name)
+                json_response.append({
+                    'id': row.pk,
+                    'name': row.name,
+                    'stats': {
+                        'attacks': row.attacks,
+                        'ws': row.ws,
+                        'dmg': row.dmg,
+                        'critical_dmg': row.dmg,
+                    },
+                    'rules': ruleses,
+                })
+        # si no tiene nombre en el header devuelve todos
+        elif request.headers.get("gunId") != None:
+            gunId = request.headers.get("gunId")
+            try:
+                gun = Gun.objects.get(id=gunId)
+            except gun.DoesNotExist:
+                return JsonResponse({"error": "Gun was not found"}, status=404)
+            rules = gun.special_rule.all()
+            ruleses = []
+            for rule in rules:
+                ruleses.append(rule.name)
+            json_response = {
+                'id': gun.pk,
+                'name': gun.name,
+                'stats': {
+                    'attacks': gun.attacks,
+                    'ws': gun.ws,
+                    'dmg': gun.dmg,
+                    'critical_dmg': gun.dmg,
+                },
+                'rules': ruleses,
+            }
+        # si no tiene nombre en el header devuelve todos
+        else:
+            all_rows = Gun.objects.all()
+            json_response = []
+            for row in all_rows:
+                json_response.append(row.to_json())
+        return JsonResponse(json_response, safe=False)
+    else:
+        return JsonResponse({'error': 'Unsupported HTTP method'}, status=405)
+
+
+def JSONLIADA(request, json_response=None, self=None):
+    # rule = SpecialRule.objects.get(id=1)
+    # Add the gun to the rule
+    # gun.special_rule.add(rule)
+    # gun.save()
+    return JsonResponse
--- warhammer/models.py
+++ warhammer/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 # Create your models here.
 class Operative(models.Model):
     name = models.CharField(max_length=240, )
@@ -13,7 +12,7 @@ class Operative(models.Model):
     w = models.IntegerField() #wounds
     base= models.IntegerField() #base diametro
     unique_action = models.ManyToManyField('UniqueAction',blank=True,null=True)
-    gun = models.ManyToManyField('Gun',blank=True)
+    #gun = models.ManyToManyField('Gun',blank=True)
     def to_json(self):
         return {
             "id": self.pk,
@@ -41,12 +40,34 @@ class Gun(models.Model):
     ws = models.IntegerField() #wounds save
     dmg = models.IntegerField() #damage standard
     critical_dmg = models.IntegerField() #critical damage
-    special_rule = models.ForeignKey('SpecialRule',blank=True,null=True,on_delete=models.DO_NOTHING)
+    special_rule = models.ManyToManyField('SpecialRule',blank=True)
+    def to_json(self):
+        return {
+            "id": self.pk,
+            "name": self.name,
+            "stats": {
+                "attacks": self.attacks,
+                "ws": self.ws,
+                "dmg": self.dmg,
+                "critical_dmg": self.critical_dmg,
+            },
+        }
+
+
+
 
 class SpecialRule(models.Model):
     name = models.CharField(max_length=240)
     description = models.CharField(max_length=1000)
     modifier = models.IntegerField(blank=True,null=True)
+    def to_json(self):
+        return {
+            "id": self.pk,
+            "name": self.name,
+            "description": self.description,
+            "modifier": self.modifier,
+        }
+
 
 class Army(models.Model):
     name = models.CharField(max_length=240,null=False,)
@@ -61,9 +82,11 @@ class CustomArmy(models.Model):
 
 class OperativeGun(models.Model):
     operative = models.ForeignKey('Operative',on_delete=models.CASCADE)
-    gun = models.ForeignKey('Gun',on_delete=models.CASCADE)
+    #gun = models.ForeignKey('Gun',on_delete=models.CASCADE)
 
 class Ability(models.Model):
     name = models.CharField(max_length=240,)
     description = models.CharField(max_length=1000)
     cost = models.IntegerField()
+
+
