3955945a00aae5be8e2dc9b9e04e671b5e607f45
anxo seoane fernandez <aseoanef23@fpcoruna.afundacion.org>
2024-11-26 19:03:43 +0100

    cambios minimos

--- wahapedia/urls.py
+++ wahapedia/urls.py
@@ -21,21 +21,20 @@ from warhammer import endpoints
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('army/',endpoints.army),
-    path('army/<int:armyId>',endpoints.armybyId),
+    path('army/<int:armyId>',endpoints.armybyId), #post
     path('operative/', endpoints.operative),
-    path('operative/<int:operativeId>', endpoints.operativebyId),
+    path('operative/<int:operativeId>', endpoints.operativebyId), #post
     path('gun/', endpoints.gun),
-    path('gun/<int:gunId>', endpoints.gunbyId),
-    path('uniqueaction/', endpoints.uniqueaction),
-    path('uniqueaction/<int:uniqueactionId>', endpoints.uniqueactionbyId),
+    path('gun/<int:gunId>', endpoints.gunbyId), #post
     #path('attack/<int:gunId>/', admin.site.urls),
+    path('uniqueaction/', endpoints.uniqueaction),
+    path('uniqueaction/<int:uniqueactionId>', endpoints.uniqueactionbyId), #post
     path('specialrule/', endpoints.specialrule),
-    path('specialrule/<int:specialruleId>', endpoints.specialrulebyId),
-    #path('army/', endpoints.army),
+    path('specialrule/<int:specialruleId>', endpoints.specialrulebyId), #post
     path('ability/', endpoints.ability),
-    path('ability/<int:abilityId>', endpoints.abilitybyId),
+    path('ability/<int:abilityId>', endpoints.abilitybyId), #post
     path('customarmy/', admin.site.urls),
-    #path('customarmy/<int:customarmyId>/', admin.site.urls),
+    #path('customarmy/<int:customarmyId>/', admin.site.urls), #get put delete post
     path('operativegun/', admin.site.urls),
-    #path('operativegun/<int:operativegunId>/', admin.site.urls),
+    #path('operativegun/<int:operativegunId>/', admin.site.urls), #post get put delete post
 ]
